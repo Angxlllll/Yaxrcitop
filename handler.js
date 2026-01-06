93e44e14a0de7123b12218465d8ca400a5667936
Angxlllll <angxlllll12@gmail.com>
2026-01-06 01:26:43 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -20,40 +20,41 @@ function isOwnerBySender(sender) {
 }
 
 global.beforeAll = async function (m, { conn }) {
-  try {
-    const nombreBot = global.namebot || "𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍"
-    const bannerFinal = "https://files.catbox.moe/izivd5.jpg"
+try {
+const nombreBot = global.namebot || '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍'
+const bannerFinal = global.bannerBuffer
 
-    const canales = [global.idcanal, global.idcanal2].filter(Boolean)
-    const newsletterJidRandom = canales.length
-      ? canales[Math.floor(Math.random() * canales.length)]
-      : null
+const canales = [global.idcanal, global.idcanal2].filter(Boolean)
+const newsletterJidRandom = canales.length
+? canales[Math.floor(Math.random() * canales.length)]
+: null
 
-    global.rcanal = {
-      contextInfo: {
-        isForwarded: true,
-        forwardingScore: 1,
-        ...(newsletterJidRandom && {
-          forwardedNewsletterMessageInfo: {
-            newsletterJid: newsletterJidRandom,
-            serverMessageId: 100,
-            newsletterName: global.namecanal
-          }
-        }),
-        externalAdReply: {
-          title: nombreBot,
-          body: global.author,
-          thumbnailUrl: bannerFinal,
-          sourceUrl: null,
-          mediaType: 1,
-          renderLargerThumbnail: false
-        }
-      }
-    }
-  } catch (e) {
-    console.log("Error al generar rcanal:", e)
-  }
+global.rcanal = {
+contextInfo: {
+isForwarded: true,
+forwardingScore: 1,
+...(newsletterJidRandom && {
+forwardedNewsletterMessageInfo: {
+newsletterJid: newsletterJidRandom,
+serverMessageId: 100,
+newsletterName: global.namecanal
 }
+}),
+externalAdReply: {
+title: nombreBot,
+body: global.author,
+thumbnail: bannerFinal,
+sourceUrl: null,
+mediaType: 1,
+renderLargerThumbnail: false
+}
+}
+}
+} catch (e) {
+console.log('Error al generar rcanal:', e)
+}
+}
+        
 
 global.dfail = (type, m, conn) => {
   const msg = {
