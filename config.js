e0e66177615e952512c29f185b1ac3ce0b9ce16b
Angxlllll <angxlllll12@gmail.com>
2026-01-06 01:12:24 -0600

    Actualizar config.js

--- config.js
+++ config.js
@@ -1,21 +1,22 @@
 import { watchFile, unwatchFile } from 'fs'
 import chalk from 'chalk'
 import { fileURLToPath } from 'url'
+import fetch from 'node-fetch'
 
 global.owner = [
-  '159606034665538',
-  '5212213479743',
-  '5215542690330',
-  '447894206349'
+'159606034665538',
+'5212213479743',
+'5215542690330',
+'447894206349'
 ]
 
 global.bot = {
-  name: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-  alias: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-  packname: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
-  author: '𝖠𝗇𝗀𝖾𝗅',
-  session: '𝖠𝗇𝗀𝖾𝗅𝖡𝗈𝗍',
-  banner: 'https://files.catbox.moe/4k94dp.jpg'
+name: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+alias: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+packname: '𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍',
+author: '𝖠𝗇𝗀𝖾𝗅',
+session: '𝖠𝗇𝗀𝖾𝗅𝖡𝗈𝗍',
+banner: 'https://files.catbox.moe/4k94dp.jpg'
 }
 
 global.namebot = global.bot.name
@@ -25,21 +26,24 @@ global.author = global.bot.author
 global.sessions = global.bot.session
 global.banner = global.bot.banner
 
+global.bannerBuffer = null
+fetch(global.banner)
+.then(r => r.arrayBuffer())
+.then(b => global.bannerBuffer = Buffer.from(b))
+.catch(() => global.bannerBuffer = null)
 
 global.APIs = {
-  sky: 'https://api-sky.ultraplus.click',
-  may: 'https://api.soymaycol.icu'
+may: 'https://api.soymaycol.icu'
 }
 
 global.APIKeys = {
-  sky: process.env.SKY_API_KEY || 'Angxlllll',
-  may: process.env.MAY_API_KEY || 'may-e89378ce'
+may: process.env.MAY_API_KEY || 'may-e89378ce'
 }
 
 const file = fileURLToPath(import.meta.url)
 
 watchFile(file, () => {
-  unwatchFile(file)
-  console.log(chalk.redBright("config.js actualizado"))
-  import(`${file}?update=${Date.now()}`)
+unwatchFile(file)
+console.log(chalk.redBright('config.js actualizado'))
+import(`${file}?update=${Date.now()}`)
 })
