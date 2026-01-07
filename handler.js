2afb9777fbce20fd4e29690ac846e05b0ad70613
Angxlllll <angxlllll12@gmail.com>
2026-01-06 23:52:03 -0600

    Actualizar handler.js

--- handler.js
+++ handler.js
@@ -21,26 +21,37 @@ function isOwnerBySender(sender) {
 
 global.beforeAll = async function (m, { conn }) {
   try {
-    const nombreBot = global.namebot || global.author
-    const bannerFinal = global.bannerBuffer
+    const nombreBot = global.namebot || "𝖠𝗇𝗀𝖾𝗅 𝖡𝗈𝗍"
+    const bannerFinal = "https://files.catbox.moe/izivd5.jpg"
 
-    if (!Buffer.isBuffer(bannerFinal)) return
+    const canales = [global.idcanal, global.idcanal2].filter(Boolean)
+    const newsletterJidRandom = canales.length
+      ? canales[Math.floor(Math.random() * canales.length)]
+      : null
 
-    await conn.sendMessage(m.chat, {
-  text: msg,
-  contextInfo: {
-    externalAdReply: {
-      title: nombreBot,
-      body: global.author,
-      thumbnail: global.bannerBuffer,
-      mediaType: 1,
-      renderLargerThumbnail: true,
-      showAdAttribution: true
+    global.rcanal = {
+      contextInfo: {
+        isForwarded: true,
+        forwardingScore: 1,
+        ...(newsletterJidRandom && {
+          forwardedNewsletterMessageInfo: {
+            newsletterJid: newsletterJidRandom,
+            serverMessageId: 100,
+            newsletterName: global.namecanal
+          }
+        }),
+        externalAdReply: {
+          title: nombreBot,
+          body: global.author,
+          thumbnailUrl: bannerFinal,
+          sourceUrl: null,
+          mediaType: 1,
+          renderLargerThumbnail: false
+        }
+      }
     }
-  }
-}, { quoted: m })
   } catch (e) {
-    console.log('Error al generar rcanal:', e)
+    console.log("Error al generar rcanal:", e)
   }
 }
         
